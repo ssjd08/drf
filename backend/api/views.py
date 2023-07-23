f42695667ac4d34e293ba607313fc8c21a567c17
sajad porkhande <>
2023-07-23 16:04:31 +0330

    [add] Product model.

---
+++ backend/api/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from products.models import Product
+
+
+def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+    return JsonResponse(data)
