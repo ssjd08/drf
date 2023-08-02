5da1471e2e99fadd187d5436d7cc6123fdb35d24
sajad porkhande <>
2023-08-02 09:44:00 +0330

    [add] make the view to API_view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,14 @@
 from django.http import JsonResponse
 from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data)
+    return Response(data)
