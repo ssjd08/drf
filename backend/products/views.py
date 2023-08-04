c9a5b0396630d3815ea631f7f7a9e6fb9abe392d
sajad porkhande <>
2023-08-04 11:44:10 +0330

    [add] product detail veiw.

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
---
+++ py_client/detail.py
@@ -0,0 +1,7 @@
+import requests
+
+
+endpoint = "http://localhost:8000/api/products/2/"
+
+get_response = requests.get(endpoint)
+print(get_response.json())
