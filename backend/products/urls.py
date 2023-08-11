4762f41ad8d5842c6ede22f6b0551744560ef28b
sajad porkhande <>
2023-08-11 18:23:56 +0330

    [add] update and destroy api views.

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.ProductListCreateAPIView.as_view(), name="List-Create-Product"),
     path('<int:pk>/', views.ProductDetailAPIView.as_view(), name="Detail-Product"),
-    path('', views.ProductListCreateAPIView.as_view(), name="Create-Product"),
+    path('destroy/<int:pk>/', views.ProductDestroyAPIView.as_view(), name="destroy-Product"),
+    path('update/<int:pk>/', views.ProductUpdateAPIView.as_view(), name="update-Product"),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -20,3 +20,24 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = 'pk'
+    
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+    
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+    
+    
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        # instance 
+        super().perform_destroy(instance)
