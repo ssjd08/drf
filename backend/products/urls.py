248a5e0da352d733d47540766a553f8ec24dfdd1
sajad porkhande <>
2023-08-11 20:08:30 +0330

    [add] ProductMixinAPIView.

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.ProductListCreateAPIView.as_view(), name="List-Create-Product"),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name="Detail-Product"),
-    path('destroy/<int:pk>/', views.ProductDestroyAPIView.as_view(), name="destroy-Product"),
-    path('update/<int:pk>/', views.ProductUpdateAPIView.as_view(), name="update-Product"),
+    path('', views.ProdoctMixinAPIView.as_view(), name="List-Create-Product"),
+    path('<int:pk>/', views.ProdoctMixinAPIView.as_view(), name="Detail-Product"),
+    path('destroy/<int:pk>/', views.ProdoctMixinAPIView.as_view(), name="destroy-Product"),
+    path('update/<int:pk>/', views.ProdoctMixinAPIView.as_view(), name="update-Product"),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,11 +1,29 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductListCreateAPIView(generics.ListCreateAPIView):
+class ProdoctMixinAPIView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView):
+    
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+    lookup_field = 'pk'
+    
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs) 
+    
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
         title = serializer.validated_data.get('title')
@@ -13,31 +31,55 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         if content is None:
             content = title
         serializer.save(content=content)
-        # send a Django signal
-   
+        # send a Django signal   
     
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'pk'
-    
-
-class ProductUpdateAPIView(generics.UpdateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'pk'
+    def update(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
     
     def perform_update(self, serializer):
         instance = serializer.save()
         if not instance.content:
             instance.content = instance.title
     
-    
-class ProductDestroyAPIView(generics.DestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = 'pk'
+    def destroy(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs) 
 
-    def perform_destroy(self, instance):
-        # instance 
-        super().perform_destroy(instance)
+# class ProductListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+#     def perform_create(self, serializer):
+#         # serializer.save(user=self.request.user)
+#         title = serializer.validated_data.get('title')
+#         content = serializer.validated_data.get('content') or None
+#         if content is None:
+#             content = title
+#         serializer.save(content=content)
+#         # send a Django signal
+   
+    
+# class ProductDetailAPIView(generics.RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'pk'
+    
+
+# class ProductUpdateAPIView(generics.UpdateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'pk'
+    
+#     def perform_update(self, serializer):
+#         instance = serializer.save()
+#         if not instance.content:
+#             instance.content = instance.title
+    
+    
+# class ProductDestroyAPIView(generics.DestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'pk'
+
+#     def perform_destroy(self, instance):
+#         # instance 
+#         super().perform_destroy(instance)
