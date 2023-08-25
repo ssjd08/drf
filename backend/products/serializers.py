e44f73b724e30ad55ba0e79079818402a6df8fbd
sajad porkhande <>
2023-08-25 12:25:15 +0330

    [add] update and detail url  to Product serializer by reverse and HyperLinkedIdentityField .

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 from .models import Product
+from rest_framework.reverse import reverse
+
 
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    update_url = serializers.SerializerMethodField(read_only=True)
+    detail_url = serializers.HyperlinkedIdentityField(view_name="product-detail",
+                                                      lookup_field="pk")
+    
+    def get_update_url (self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        
+        return reverse('product-update', kwargs={"pk":obj.pk}, request= request)
+    
     def get_my_discount(self, obj):
         if not hasattr(obj, 'id'):
             return None
@@ -14,6 +27,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
+            'detail_url',
+            'update_url',
             'pk',
             'title',
             'content',
